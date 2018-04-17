edad89dcb90be50afc07c3874d3939f3e37cd683
Haknima <33159334+Haknima@users.noreply.github.com>
2018-04-17 11:18:30 +0800

    一键脚本

--- transmissionforpt.sh
+++ transmissionforpt.sh
@@ -12,7 +12,96 @@ echo "# Author: Haknima                                         #"
 echo "###########################################################"
 echo
 
+#清空删除transmission
+service transmissiond stop
+killall -9 transmission-da
+rm -rf /home/transmission
+rm -rf /usr/share/transmission
+rm -rf /etc/init.d/transmissiond
+
 #依赖
 yum install -y wget xz gcc gcc-c++ m4 make automake libtool gettext openssl-devel pkgconfig perl-libwww-perl perl-XML-Parser curl curl-devel libidn-devel zlib-devel which libevent
 yum install -y zlib zlib-devel readline-devel sqlite sqlite-devel openssl-devel mysql-devel gd-devel openjpeg-devel
 
+#依赖包
+cd /root
+wget https://github.com/Haknima/One-click-pt/raw/master/intltool-0.40.6.tar.gz
+tar -zxf intltool-0.40.6.tar.gz
+cd intltool-0.40.6
+./configure --prefix=/usr
+make -s
+make -s install
+
+cd /root
+wget https://github.com/Haknima/One-click-pt/raw/master/libevent-2.0.21-stable.tar.gz
+tar -zxf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+./configure
+make -s
+make -s install
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+ln -s /usr/local/lib/libevent-2.0.so.5 /usr/lib/libevent-2.0.so.5
+ln -s /usr/local/lib/libevent-2.0.so.5.1.9 /usr/lib/libevent-2.0.so.5.1.9
+ln -s /usr/lib/libevent-2.0.so.5 /usr/local/lib/libevent-2.0.so.5
+ln -s /usr/lib/libevent-2.0.so.5.1.9 /usr/local/lib/libevent-2.0.so.5.1.9
+
+#主程序
+cd /root
+wget https://github.com/Haknima/One-click-pt/raw/master/transmission-2.84.tar.gz
+tar -zxf transmission-2.84.tar.gz
+cd transmission-2.84
+./configure --prefix=/usr
+make -s
+make -s install
+
+#配置文件
+cd /root
+useradd -m transmission
+passwd -d transmission
+mv transmission.sh /etc/init.d/transmissiond
+chmod 755 /etc/init.d/transmissiond
+chkconfig --add transmissiond
+chkconfig --level 2345 transmissiond on
+mkdir -p /home/transmission/Downloads/
+chmod g+w /home/transmission/Downloads/
+mkdir -p /home/transmission/.config/transmission/
+wget https://github.com/Haknima/One-click-pt/raw/master/settings.json
+mv -f settings.json /home/transmission/.config/transmission/settings.json
+chown -R transmission.transmission /home/transmission
+
+#修改配置信息
+read -p "请输入 transmission 账号 (默认账号:zhangha): " UserName
+read -p "请输入 transmission 密码 (默认密码:mima): " PassWord
+read -p "请输入 transmission 端口 (默认端口:9091): " Port
+sed -i "s#zhangha#${UserName}#" /home/transmission/.config/transmission/settings.json
+sed -i "s#mima#${PassWord}#" /home/transmission/.config/transmission/settings.json
+sed -i "s#9091#${Port}#" /home/transmission/.config/transmission/settings.json
+
+#防火墙规则
+iptables -t nat -F
+iptables -t nat -X
+iptables -t nat -P PREROUTING ACCEPT
+iptables -t nat -P POSTROUTING ACCEPT
+iptables -t nat -P OUTPUT ACCEPT
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t mangle -P PREROUTING ACCEPT
+iptables -t mangle -P INPUT ACCEPT
+iptables -t mangle -P FORWARD ACCEPT
+iptables -t mangle -P OUTPUT ACCEPT
+iptables -t mangle -P POSTROUTING ACCEPT
+iptables -F
+iptables -X
+iptables -P FORWARD ACCEPT
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t raw -F
+iptables -t raw -X
+iptables -t raw -P PREROUTING ACCEPT
+iptables -t raw -P OUTPUT ACCEPT
+service iptables save
+
+#安装美化
+wget https://github.com/ronggang/transmission-web-control/raw/master/release/install-tr-control-cn.sh
+bash install-tr-control-cn.sh
+service transmissiond start
